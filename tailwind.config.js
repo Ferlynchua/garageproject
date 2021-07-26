d0edfe0a78b6b0139195a7ab538dea7b820b2a2c
kingsleylow <65750383+kingsleylow@users.noreply.github.com>
2021-07-27 00:35:08 +0800

    feat: added design and more hk

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'hk-ribbon': "url('https://gateway.pinata.cloud/ipfs/QmSVVn7y5QPAXhvxw5pscFBfkMCZA3EL551ojm6bmd53cU/hk%20bg/hkwall3.jpg')",
+        'footer-texture': "url('https://gateway.pinata.cloud/ipfs/QmSVVn7y5QPAXhvxw5pscFBfkMCZA3EL551ojm6bmd53cU/hk%20bg/hkwall4.jpg')",
+      })
+    },
   },
   variants: {
     extend: {},
